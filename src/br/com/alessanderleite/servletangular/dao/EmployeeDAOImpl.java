8b8901d70120669e99c370287b3005e5b3016c4f
alessanderleite <alessander.logistica@gmail.com>
2019-09-22 18:42:55 -0300

    create the class EmployeeDAOImpl

---
+++ src/br/com/alessanderleite/servletangular/dao/EmployeeDAOImpl.java
@@ -0,0 +1,19 @@
+package br.com.alessanderleite.servletangular.dao;
+
+import java.util.List;
+
+import br.com.alessanderleite.servletangular.beans.Employee;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		return null;
+	}
+
+	@Override
+	public int save(Employee employee) {
+		return 0;
+	}
+
+}
