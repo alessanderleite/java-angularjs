a0d7d109ddee86e7db1b7fd3696ecc9553699434
alessanderleite <alessander.logistica@gmail.com>
2019-09-22 19:33:15 -0300

    update dao

--- src/br/com/alessanderleite/servletangular/dao/EmployeeDAO.java
+++ src/br/com/alessanderleite/servletangular/dao/EmployeeDAO.java
@@ -1,10 +1,12 @@
 package br.com.alessanderleite.servletangular.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import br.com.alessanderleite.servletangular.beans.Employee;
 
 public interface EmployeeDAO {
 	
-	List<Employee> findAll();
+	List<Employee> findAll() throws SQLException;
+	int save(Employee employee);
 }
--- src/br/com/alessanderleite/servletangular/dao/EmployeeDAOImpl.java
+++ src/br/com/alessanderleite/servletangular/dao/EmployeeDAOImpl.java
@@ -1,14 +1,36 @@
 package br.com.alessanderleite.servletangular.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alessanderleite.servletangular.beans.Employee;
+import br.com.alessanderleite.servletangular.dao.util.ConnectionUtil;
+import br.com.alessanderleite.servletangular.dao.util.SQLQuery;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
 
 	@Override
-	public List<Employee> findAll() {
-		return null;
+	public List<Employee> findAll() throws SQLException {
+		
+		List<Employee> empList = new ArrayList<Employee>();
+		Connection con = ConnectionUtil.getNewConnection();
+		PreparedStatement ps = con.prepareStatement(SQLQuery.SQL_SELECT_ALL_EMPLOYEE);
+		ResultSet rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			int empId = rs.getInt(1);
+			String name = rs.getString(2);
+			double salary = rs.getDouble(3);
+			String designation = rs.getString(4);
+			String address = rs.getString(5);
+			Employee employee = new Employee(empId, name, salary, designation, address);
+			empList.add(employee);
+		}
+		return empList;
 	}
 
 	@Override
