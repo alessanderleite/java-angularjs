d6d8419cdedbe9796587a9a116863b43fc9e8649
alessanderleite <alessander.logistica@gmail.com>
2019-09-22 19:01:30 -0300

    create the class named SQLQuery

---
+++ src/br/com/alessanderleite/servletangular/dao/util/SQLQuery.java
@@ -0,0 +1,6 @@
+package br.com.alessanderleite.servletangular.dao.util;
+
+public class SQLQuery {
+
+	public static final String SQL_SELECT_ALL_EMPLOYEE="select emp_id, name, salary, desgination, adrress from employee";
+}
