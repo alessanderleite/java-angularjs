a81a674a36b9fc57e494425fb03894ed90d4335b
alessanderleite <alessander.logistica@gmail.com>
2019-09-22 18:36:55 -0300

    create the classes and interfaces

---
+++ src/br/com/alessanderleite/servletangular/beans/Employee.java
@@ -0,0 +1,61 @@
+package br.com.alessanderleite.servletangular.beans;
+
+public class Employee {
+
+	private int empId;
+	private String name;
+	private double salary;
+	private String designation;
+	private String address;
+	
+	public Employee() {}
+
+	public Employee(int empId, String name, double salary, String designation, String address) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.salary = salary;
+		this.designation = designation;
+		this.address = address;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
---
+++ src/br/com/alessanderleite/servletangular/dao/EmployeeDAO.java
@@ -0,0 +1,5 @@
+package br.com.alessanderleite.servletangular.dao;
+
+public interface EmployeeDAO {
+
+}
